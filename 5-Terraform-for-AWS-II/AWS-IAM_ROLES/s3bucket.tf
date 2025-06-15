e60e5c4a93c69f6f91ae07aa8a37ccd20ba34a90
Akshay Devanpally <akshaydevanpally@gmail.com>
2025-06-15 17:47:59 +0530

    update

--- 5-Terraform-for-AWS-II/AWS-IAM_ROLES/s3bucket.tf
+++ 5-Terraform-for-AWS-II/AWS-IAM_ROLES/s3bucket.tf
@@ -21,9 +21,6 @@ resource "aws_s3_bucket" "levelup-s3bucket" {
 # --------------------------------------------------------------------------------------------
 resource "aws_s3_bucket_acl" "levelup-s3bucket-acl" {
   bucket = aws_s3_bucket.levelup-s3bucket.id
-  
-  rule {
-    object_ownership = "BucketOwnerEnforced"
-  }
+  acl    = "public-read"
 }
 
